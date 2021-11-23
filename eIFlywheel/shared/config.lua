d1f09b0a83033be1d5fe22a298be15884d932538
eDen <85058066+eDenYZ@users.noreply.github.com>
2021-11-23 17:44:27 +0100

    Update config.lua

--- eIFlywheel/shared/config.lua
+++ eIFlywheel/shared/config.lua
@@ -1,33 +1,58 @@
 eIFlywheels = {
     KitPosition = {
-        {pos = vector3(1765.5823974609, 3332.2153320312, 41.428588867188)},
+        {pos = vector3(1765.5823974609, 3332.2153320312, 41.428588867188)}
     },
     CustomPosition = {
         {pos = vector3(1767.8901367188, 3329.3273925781, 40.788330078125)}
     },
     BlipsJob = {
         Blip = {
-            {pos = vector3(1768.1669921875, 3330.1450195312, 40.822021484375), id = 72, scale = 0.7, display = 4, color = 0, name = "Flywheels"},
-        },
+            {
+                pos = vector3(1768.1669921875, 3330.1450195312, 40.822021484375),
+                id = 72,
+                scale = 0.7,
+                display = 4,
+                color = 0,
+                name = "Flywheels"
+            }
+        }
     },
     PedJob = {
         Ped = {
-            {PedName = "s_m_m_autoshop_02", pos = vector4(1776.5142822266, 3322.4306640625, 40.428588867188, 25.511812210083)},
-        },
+            {
+                PedName = "s_m_m_autoshop_02",
+                pos = vector4(1776.5142822266, 3322.4306640625, 40.428588867188, 25.511812210083)
+            }
+        }
     },
     Kit = {
         {Nom = "Kit Moteur", Item = "moteurkit"},
         {Nom = "Kit Carrosserie", Item = "carokit"},
-        {Nom = "Bouteille de gaz", Item = "gazbottle"}, 
-        {Nom = "Chiffon", Item = "chiffon"},
+        {Nom = "Bouteille de gaz", Item = "gazbottle"},
+        {Nom = "Chiffon", Item = "chiffon"}
     },
     GaragePosition = {
-        {pos = vector3(1777.859375, 3324.5144042969, 41.428588867188)},
+        {pos = vector3(1777.859375, 3324.5144042969, 41.428588867188)}
     },
     Garage = {
-        {Label = "Plateau", Name = "flatbed", PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375), HeadingVeh = 215.43309020996,},
-        {Label = "Petite Dépaneuse", Name = "towtruck2", PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375), HeadingVeh = 215.43309020996,},
-        {Label = "Dépaneuse", Name = "towtruck", PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375), HeadingVeh = 215.43309020996,},
+        {
+            Label = "Plateau",
+            Name = "flatbed",
+            PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375),
+            HeadingVeh = 215.43309020996
+        },
+        {
+            Label = "Petite Dépaneuse",
+            Name = "towtruck2",
+            PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375),
+            HeadingVeh = 215.43309020996
+        },
+        {
+            Label = "Dépaneuse",
+            Name = "towtruck",
+            PositionVeh = vector3(1780.5362548828, 3330.3295898438, 41.226318359375),
+            HeadingVeh = 215.43309020996
+        }
     },
     VehiculeSuppression = {
         {pos = vector3(1780.5362548828, 3330.3295898438, 41.226318359375)}
@@ -40,29 +65,42 @@ eIFlywheels = {
     },
     Tenues = {
         male = {
-            ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-            ['torso_1'] = 65,   ['torso_2'] = 0,
-            ['decals_1'] = 0,   ['decals_2'] = 0,
-            ['arms'] = 16,
-            ['pants_1'] = 38,   ['pants_2'] = 0,
-            ['shoes_1'] = 25,   ['shoes_2'] = 0,
-            ['helmet_1'] = -1,  ['helmet_2'] = 0,
-            ['chain_1'] = 0,    ['chain_2'] = 0
+            ["tshirt_1"] = 15,
+            ["tshirt_2"] = 0,
+            ["torso_1"] = 65,
+            ["torso_2"] = 0,
+            ["decals_1"] = 0,
+            ["decals_2"] = 0,
+            ["arms"] = 16,
+            ["pants_1"] = 38,
+            ["pants_2"] = 0,
+            ["shoes_1"] = 25,
+            ["shoes_2"] = 0,
+            ["helmet_1"] = -1,
+            ["helmet_2"] = 0,
+            ["chain_1"] = 0,
+            ["chain_2"] = 0
         },
         female = {
-            ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-            ['torso_1'] = 65,   ['torso_2'] = 0,
-            ['decals_1'] = 0,   ['decals_2'] = 0,
-            ['arms'] = 16,
-            ['pants_1'] = 38,   ['pants_2'] = 0,
-            ['shoes_1'] = 25,   ['shoes_2'] = 0,
-            ['helmet_1'] = -1,  ['helmet_2'] = 0,
-            ['chain_1'] = 0,    ['chain_2'] = 0
-        },
+            ["tshirt_1"] = 15,
+            ["tshirt_2"] = 0,
+            ["torso_1"] = 65,
+            ["torso_2"] = 0,
+            ["decals_1"] = 0,
+            ["decals_2"] = 0,
+            ["arms"] = 16,
+            ["pants_1"] = 38,
+            ["pants_2"] = 0,
+            ["shoes_1"] = 25,
+            ["shoes_2"] = 0,
+            ["helmet_1"] = -1,
+            ["helmet_2"] = 0,
+            ["chain_1"] = 0,
+            ["chain_2"] = 0
+        }
     },
     Notif = {
         Char = "CHAR_LAZLOW",
-        ShowHelpNotification = "Appuyer sur ~INPUT_CONTEXT~ pour intéragir",
+        ShowHelpNotification = "Appuyer sur ~INPUT_CONTEXT~ pour intéragir"
     }
 }
-
